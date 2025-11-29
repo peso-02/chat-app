b500344a78c53283e2f96abf6eebb8fc54e9c651
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2025-11-29 12:22:57 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   root to: "messages#index"
 end
